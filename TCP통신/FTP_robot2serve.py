1c6ee7cd7b9dc3e9b9799c5d478ec037d9be6e20
Taein Kim <91868163+taeinkimkim@users.noreply.github.com>
2024-08-08 20:27:55 +0900

    Update FTP_robot2serve.py

--- TCP통신/FTP_robot2serve.py
+++ TCP통신/FTP_robot2serve.py
@@ -1,27 +1,29 @@
 import ftplib
 import os
 
-HOST = ''
-PORT = 1234
-ID = '' 
-PW = '' 
+HOST = '192.168.0.55'
+PORT = 21
+ID = 'mines' 
+PW = 'mines' 
 
 f = ftplib.FTP() 
 f.set_debuglevel(1) 
 
 f.connect(HOST,PORT) 
+print('connected!')
 
 f.login(ID,PW) 
+print('login!')
 
 entries = f.nlst() 
 print(entries)
 dir = f.dir() 
 print(dir)
 
-f.cwd('') 
+f.cwd('/home/mines/Desktop/chat/') 
 
 
-file_path = ''
+file_path = '/home/ubuntu/Desktop/mines_lab/mines_robot/MIC/'
 
 filename = os.path.join(file_path,'output.wav')
 with open(filename, 'rb') as read_f:
