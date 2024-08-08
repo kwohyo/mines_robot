abc6791c1b797707878e11e12bc2d8e82f77f6b0
hyojeong <78974068+kwohyo@users.noreply.github.com>
2024-08-08 17:03:38 +0900

    Update pills.py

--- pill/pills.py
+++ pill/pills.py
@@ -1,7 +1,7 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO('C:/MINESLAB/yolov8-face/runs/detect/train2/weights/best.pt')
+model = YOLO('C:/MINESLAB/yolov8-face/runs/detect/train2/weights/best.pt') # best.pt의 경로
 
 cap=cv2.VideoCapture(0)
 
