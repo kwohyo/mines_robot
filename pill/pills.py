29b8b07fbbaf5aeb1fcf35d35f35af92ea9da2e8
hyojeong <78974068+kwohyo@users.noreply.github.com>
2024-08-08 17:02:23 +0900

    Add files via upload

---
+++ pill/pills.py
@@ -0,0 +1,23 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO('C:/MINESLAB/yolov8-face/runs/detect/train2/weights/best.pt')
+
+cap=cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, frame=cap.read()
+
+    if success:
+        results=model(frame)
+        annotated_frame=results[0].plot()
+        cv2.imshow("YOLOv8 Inference",annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF==ord("q"):
+            break
+
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
